fbc3c931feec6c5d3834c86e52a0a7f2e96c51ed
daverees4 <d.w.rees.03@gmail.com>
2017-08-25 16:07:10 +0200

    First Commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+(function() {
+  var hint_count = 0;
+  document.getElementById("hintbutton").addEventListener("click", function() {
+    document.getElementById("hint_1").style.height = "100%";
+  });
+})();
